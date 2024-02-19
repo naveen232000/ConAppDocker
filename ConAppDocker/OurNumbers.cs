5fb49177856452d887eb2f300de8857ca74c863b
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-19 13:07:05 +0530

    Add project files.

---
+++ ConAppDocker/OurNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDocker
+{
+    public  class OurNumbers
+    {
+        public static void Tables(int num)
+        {
+            Console.WriteLine($"Table for {num}");
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine($"{num,-3} * {i,-3} = {(num*i),-3}");
+                Thread.Sleep(500);
+            }
+          
+        }
+        public static void CountingNum(int lnum)
+        {
+            Console.WriteLine($"Counting from 1 to {lnum}");
+            for (int i = 1;i <= lnum; i++)
+            {
+                Console.Write($"{i,-3}");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConAppDocker/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConAppDocker;
+
+Console.WriteLine("****Welcome to Number System ****");
+int lnum=int.Parse(Console.ReadLine());
+OurNumbers.CountingNum(lnum);
+Console.WriteLine("Enter table number");
+int num=int.Parse(Console.ReadLine());
+OurNumbers.Tables(num);
